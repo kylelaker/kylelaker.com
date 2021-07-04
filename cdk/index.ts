63f153a30a45f9925659d4145a0749d5a4abe805
Kyle Laker <kyle@laker.email>
2021-07-04 09:00:48 -0400

    Add CDK configuration

--- cdk/index.ts
+++ cdk/index.ts
@@ -4,27 +4,27 @@ import { StaticSite } from './static-site';
 /*
  * This code largely comes from:
  * https://github.com/aws-samples/aws-cdk-examples/tree/master/typescript/static-site
- * 
+ *
  * It is a goal to clean this up and customize it a bit more in the future.
  */
 
 class StaticSiteStack extends cdk.Stack {
-    constructor(parent: cdk.App, name: string, props: cdk.StackProps) {
-        super(parent, name, props);
+  constructor(parent: cdk.App, name: string, props: cdk.StackProps) {
+    super(parent, name, props);
 
-        new StaticSite(this, 'StaticSite', {
-            domainName: this.node.tryGetContext('domain'),
-        });
-    }
+    new StaticSite(this, 'StaticSite', {
+      domainName: this.node.tryGetContext('domain'),
+    });
+  }
 }
 
 const app = new cdk.App();
 
 new StaticSiteStack(app, 'BuildStaticSite', {
-    env: {
-        account: app.node.tryGetContext('accountId'),
-        region: 'us-east-1',
-    }
+  env: {
+    account: app.node.tryGetContext('accountId'),
+    region: 'us-east-1',
+  },
 });
 
 app.synth();
