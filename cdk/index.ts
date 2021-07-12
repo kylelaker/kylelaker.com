278a4b472149e92f0a5729790f74d58451cfab9b
Kyle Laker <kyle@laker.email>
2021-07-12 01:54:07 -0400

    Update package metadata
    This is no longer a pure copy & paste from the AWS sample repo.

--- cdk/index.ts
+++ cdk/index.ts
@@ -1,13 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import { StaticSite } from './static-site';
 
-/*
- * This code largely comes from:
- * https://github.com/aws-samples/aws-cdk-examples/tree/master/typescript/static-site
- *
- * It is a goal to clean this up and customize it a bit more in the future.
- */
-
 class StaticSiteStack extends cdk.Stack {
   constructor(parent: cdk.App, name: string, props: cdk.StackProps) {
     super(parent, name, props);
